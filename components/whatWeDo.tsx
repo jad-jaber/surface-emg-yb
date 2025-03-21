72d67a9ea964d0aecb954f37c8cdf9a9dceb8a10
Jad Jaber <jabe0043@algonquinlive.com>
2025-03-21 18:56:38 -0400

    adjust what we do component height

--- components/whatWeDo.tsx
+++ components/whatWeDo.tsx
@@ -4,15 +4,17 @@ export default function WhatWeDo(){
 
 
   return(
-  <div className="width-full flex justify-center items-center h-full">
-    <div className="flex p-[24px] flex-col md:flex-column items-center justify-center md:w-1/2 space-y-4 md:space-y-0 md:space-x-4">
+    <div className="md:h-[50vh] flex justify-center items-center">
+      <div className="flex p-[24px] flex-col md:flex-column items-center justify-center md:w-1/2 space-y-4 md:space-y-0 md:space-x-4">
         <div>
           <h2 className="text-xl font-semibold">WHAT WE DO</h2>
         </div>
         <div className="flex-shrink-0 md:w-full">
-          <p className="text-gray-500 text-left">Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias porro blanditiis sunt aspernatur nesciunt quibusdam ipsum id eius! Porro consequatur veritatis debitis error dolorem.</p>
+          <p className="text-gray-500 text-left">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias porro blanditiis sunt aspernatur nesciunt quibusdam ipsum id eius! Porro consequatur veritatis debitis error dolorem.
+          </p>
         </div>
+      </div>
     </div>
-  </div>
   )
 }
